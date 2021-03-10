e6adbcc00c78b32d39e0c5c7b550c34a15367116
Peiyu Quan <41603034+PeiyuQuan@users.noreply.github.com>
2021-03-10 02:42:44 -0500

    Add files via upload

--- iocBoot/iocDC/st.cmd
+++ iocBoot/iocDC/st.cmd
@@ -1,35 +1,35 @@
-#!../../bin/linux-x86_64/E3633A
+#!../../bin/linux-x86_64/AJA
 
-#- You may have to change E3633A to something else
+#- You may have to change AJA to something else
 #- everywhere it appears in this file
 
 < envPaths
-
-
-epicsEnvSet(STREAM_PROTOCOL_PATH,"$(TOP)/DCApp/Db")
-epicsEnvSet("PREFIX", "UWXRD:E3633A_1:")
+epicsEnvSet ("STREAM_PROTOCOL_PATH", "${TOP}/AJAApp/Db")
+epicsEnvSet("PREFIX", "UWXRD:AJA:")
 epicsEnvSet("PORT", "serial1")
 
 
 ## Register all support components
-dbLoadDatabase "$(TOP)/dbd/DC.dbd"
-DC_registerRecordDeviceDriver(pdbbase)
-drvAsynSerialPortConfigure("serial1","/dev/ttyUSB0",0,0,0)
-asynOctetSetInputEos("serial1",0,"\n")
-asynOctetSetOutputEos("serial1",0,"\n")
-asynSetOption("serial1",0,"baud","1200")
+dbLoadDatabase "${TOP}/dbd/AJA.dbd"
+AJA_registerRecordDeviceDriver pdbbase
+drvAsynSerialPortConfigure("serial1", "ttyUSB0", 0, 0, 0)
+asynSetOption("serial1",0,"baud","38400")
 asynSetOption("serial1",0,"bits","8")
-asynSetOption("serial1",0,"stop","2")
+asynSetOption("serial1",0,"stop","1")
 asynSetOption("serial1",0,"parity","none")
-asynSetOption("serial1",0,"clocal","N")
+asynSetOption("serial1",0,"clocal","Y")
 asynSetOption("serial1",0,"crtscts","N")
+asynSetTraceIOMask("serial1",0, ESCAPE|HEX)
+
+
 asynSetTraceIOMask("serial1",0,2)
 asynSetTraceMask("serial1",0,9)
 
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=uwchamber")
-dbLoadRecords("$(TOP)/db/Agilent_E3633A.db","P=$(PREFIX),PORT=serial1")
-#cd "/home/uwchamber/epics/DC/iocBoot/iocDC"
+dbLoadRecords("${TOP}/AJAApp/Db/AJA-powersupply.db","P=$(PREFIX),PORT=serial1")
+
+
 iocInit
 
 ## Start any sequence programs
