c57ebe854018ce2c9425e39bb51a42b8debb17cc
Quan, Peiyu <41603034+PeiyuQuan@users.noreply.github.com>
2022-10-11 16:17:25 -0700

    Update st.cmd

--- iocBoot/iocE3633A/st.cmd
+++ iocBoot/iocE3633A/st.cmd
@@ -27,21 +27,12 @@ asynSetOption("serial1",0,"parity","none")
 asynSetOption("serial1",0,"clocal","N")
 asynSetOption("serial1",0,"crtscts","N")
 
-## Load record instances
 
 dbLoadRecords ("${TOP}/E3633AApp/Db/Agilent_E3633A.db", "P=$(PREFIX), PORT=serial1")
 dbLoadRecords ("$(ASYN)/db/asynRecord.db", "P=$(PREFIX), R=asyn1, PORT=serial1, ADDR=0, IMAX=80, OMAX=80")
 dbLoadRecords ("$(AUTOSAVE)/db/save_restoreStatus.db", "P=SLAC:E3633A:")
 
 
-#- Set this to see messages from mySub
-#var mySubDebug 1
-
-#- Run this to trace the stages of iocInit
-#traceIocInit
-
 #cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
-## Start any sequence programs
-#seq sncExample, "user=quan"
